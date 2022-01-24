85a75598c857b24c56311b233bc57ca696657954
theerudito <erudito.tv@gmail.com>
2022-01-24 17:09:47 -0500

    v3

--- app.js
+++ app.js
@@ -17,15 +17,15 @@ window.addEventListener("load", () => {
 
 
         //Ubicacion Actual
-        const ciudad  = `http://api.openweathermap.org/data/2.5/weather?lang=es&units=metric&lat=${latitud}&lon=${longitud}&appid=6a3b130f8cc2a2577fac82539b3ca3cb`
-        console.log(ciudad);
+       const ciudad  = `http://api.openweathermap.org/data/2.5/weather?lang=es&units=metric&lat=${latitud}&lon=${longitud}&appid=6a3b130f8cc2a2577fac82539b3ca3cb`
+       console.log(ciudad);
 
 
         // Unicacion Por Ciudad
         const pais = `http://api.openweathermap.org/data/2.5/weather?q=Ecuador&lang=es&units=metric&appid=6a3b130f8cc2a2577fac82539b3ca3cb`
         
 
-        fetch(pais)
+        fetch(ciudad)
           .then(res => { return res.json()})
           .then(data => {
           
@@ -41,23 +41,44 @@ window.addEventListener("load", () => {
             let velocidad = data.wind.speed
             vientoVelocidad.textContent = `${velocidad} m/s`
             
-
+              console.log(data);
             // ICONOS ANIMADOS
             console.log(data.weather[0].main)
-            switch (data.weather[0].main) {
-              case "Clear":
-                  iconoAnimado.src = 'iconos/day.svg'
-                  console.log("DESPEJADO");
-                break;
-              case "Clouds":
-                    iconoAnimado.src = 'iconos/cloudy-day-1.svg'
-                    console.log("NUBES");
-                break; 
-              case "Rain":
-                    iconoAnimado.src = 'iconos/rainy-2.svg'
-                    console.log("LLOVIENDO");
-                break;  
-            }
+                switch (data.weather[0].main) {
+                    case 'Thunderstorm':
+                      iconoAnimado.src='iconos/thunder.svg'
+                      console.log('TORMENTA');
+                      break;
+                    case 'Drizzle':
+                      iconoAnimado.src='iconos/rainy-2.svg'
+                      console.log('LLOVIZNA');
+                      break;
+                    case 'Rain':
+                      iconoAnimado.src='iconos/rainy-7.svg'
+                      console.log('LLUVIA');
+                      break;
+                    case 'Snow':
+                      iconoAnimado.src='iconos/snowy-6.svg'
+                        console.log('NIEVE');
+                      break;                        
+                    case 'Clear':
+                        iconoAnimado.src='iconos/day.svg'
+                        console.log('LIMPIO');
+                      break;
+                    case 'Atmosphere':
+                      iconoAnimado.src='iconos/weather.svg'
+                        console.log('ATMOSFERA');
+                        break;  
+                    case 'Clouds':
+                        iconoAnimado.src='iconos/cloudy-day-1.svg'
+                        console.log('NUBES');
+                        break;  
+                    default:
+                      iconoAnimado.src='iconos/cloudy-day-1.svg'
+                      console.log('por defecto');
+                  }
+
+          
             // ICONOS ANIMADOS
 
           })
